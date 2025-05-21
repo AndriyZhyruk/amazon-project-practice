8f8c02648eba2325be031b390b2d0b15dfb6637e
Texztech <movchanandriy6@gmail.com>
2025-05-21 23:15:17 +0200

    Make the Added text visible

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -4,53 +4,53 @@ products.forEach(product => {
   productsHTML +=
     `
       <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
-
-          <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector-${product.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+  
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+  
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+  
+        <div class="product-price">
+          $${(product.priceCents / 100).toFixed(2)}
+        </div>
+  
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+  
+        <div class="product-spacer"></div>
+  
+        <div class="added-to-cart js-added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+  
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+          Add to Cart
+        </button>
+      </div>
     `
 });
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
@@ -61,6 +61,12 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
 
     const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`).value
     const quantity = Number(quantitySelector);
+
+    const addedToCart = button.parentElement.querySelector('.js-added-to-cart');
+    addedToCart.classList.add('visible');
+    setTimeout(() => {
+      addedToCart.classList.remove('visible');
+    }, 1500);
 
     let matchingItem;
 
