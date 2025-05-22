4c46c6e64253221d39e134743bc0421f4547ba96
Texztech <movchanandriy6@gmail.com>
2025-05-22 20:04:55 +0200

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,30 @@
 export const cart = [];
+
+export function addToCart (productId, button) {
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`).value
+  const quantity = Number(quantitySelector);
+
+  const addedToCart = button.parentElement.querySelector('.js-added-to-cart');
+  addedToCart.classList.add('visible');
+  setTimeout(() => {
+    addedToCart.classList.remove('visible');
+  }, 1500);
+
+  let matchingItem;
+
+  cart.forEach(cartItem => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  }
+  else {
+    cart.push({
+      productId,
+      quantity,
+    });
+  }
+}
